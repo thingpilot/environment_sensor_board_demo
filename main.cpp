d1f3048a14847362ffe642082d4b969acb543b81
Rafaella Neofytou <rafaella@think-engineer.com>
2019-10-09 15:15:57 +0100

    Updating the example program

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
   * @file    main.cpp
   * @version 1.0.0
   * @author  
-  * @brief    
+  * @brief    Example of an application using the Think Pilot hardware/software
   * 
   */
 
@@ -10,66 +10,63 @@
  */
 #include "main.h"
 
+#include "Dummy_Sensor_1.h"
+#include "Dummy_Sensor_2.h"
+
 NodeFlow mydevice;
-//HDT1080 HDT1080(); 
-int standby_time=0;
+
+//uint8_t NumofSensors=2;
+uint8_t sensorId[]={D1,D2};
+uint8_t seadingTime_h[]={5,10};
+
+
+Dummy_Sensor_1 SensorId[0];
+
+//int standby_time=0;
 
 
 int main()
 {
 
  mydevice.start();
- //optional
- if (mydevice.alivebeat()==0){
-    printf("\r\nSuccess\r\n");
- }
- else {
-     printf("\r\nUnsuccess\r\n");
- }
- 
+ /**Wakeup type
+  */
  WakeupType wakeup = mydevice.get_wakeup_type();
+ //Initialisation/setup
  if (wakeup==WAKEUP_RESET) {
        
        mydevice.reset();
-       //after first wakeup sleep 5 mins without interrupt from pin
+ /**After first wakeup from interrupt, sleep 5 mins without interrupt from pin
+  * 
+  */
        mydevice.set_max_interrupt_sleep(5);
 
-/*I need to re-think about the following process
-       Presentation- each sensor has to present itself before starting to report data
-        
-       
-        uint8_t numSensors=3;
-        uint8_t childSensorId[numSensors];
-        uint8_t sensorType[numSensors];
-        uint8_t readingTime_h[numSensors];
-        const char *description=NULL;
-        bool echo=true;
-        mydevice.present(childSensorId[1],sensorType[1],readingTime_h[1],"Temp sensor/every hour",echo);
-       //set devices each sensor type has a specific value for 1:Temp_C 
-       mydevice.set_sensors(1,2);
-       
-*/
-
-/**Calculates & returns the next sensor reading time defined by the user 
-  *Finds the smallest interval, substracts from the second smallest 
-  */
-       standby_time=mydevice.next_reading_time();
-
+/** Register each sensor to the eeprom
+ *  Each sensor has to register to the EEPROM before starting to report data  
+ */
+       for (int i=0; i<sizeof(SensorId); i++ ) {
+       mydevice.registerSensor(sensorId[i],seadingTime_h[i]);
        }
+
+//Calculates & returns the next sensor reading time defined by the user 
+       standby_time=mydevice.set_reading_time();
+
+  }
  if (wakeup==WAKEUP_TIMER) {
        
        //if sensor/Check which sensor else just kick the wdg
        mydevice.SensorsHandler();
        printf("Sensor Reading time");
        //User specific application
-       standby_time=mydevice.next_reading_time();       
+       //standby_time=mydevice.next_reading_time();       
    }
  if (wakeup==WAKEUP_PIN) {
        printf("Trigger");
        //user specific application
-       standby_time=mydevice.next_reading_time();
-        }
+       //standby_time=mydevice.next_reading_time();
+   }
  
-    mydevice.enter_standby(standby_time); 
+
+  mydevice.enter_standby(standby_time); 
 }
 
